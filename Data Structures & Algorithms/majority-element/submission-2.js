ddfd6399cfcdaf0596168228aa5796593f4bc10d
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-06-02 06:09:09 +0530

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    majorityElement(nums) {
+        let count = 0;
+        let candidate = null;
+
+        for(let i = 0; i<nums.length; i++){
+            if(count === 0){
+                candidate = nums[i];
+                count++;
+            }else if(nums[i] === candidate){
+                count++;
+            }else{
+                count--;
+            }
+        }
+
+        return candidate;
+    }
+}
