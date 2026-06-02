6dd41c1ef432c0dd81d8d25a9e853e4fecc4a308
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-06-02 14:24:28 +0530

    Add: counting-bits - submission-0

---
+++ Data Structures & Algorithms/counting-bits/submission-0.js
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number[]}
+     */
+    countBits(n) {
+        let res = new Array(n+1).fill(0);
+        res[0] = 0
+        for(let i = 1; i<=n; i++){
+            res[i] = res[i >> 1] + (i & 1);
+        }
+
+        return res;
+    }
+}
